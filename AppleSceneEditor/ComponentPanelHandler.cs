d5a1f7a92c61c76ab127e11ac61ddbb77829e7ec
qapples <793678@lcps.org>
2021-09-03 11:48:52 -0400

    Introduce ValueEditorFactory.cs

--- AppleSceneEditor/ComponentPanelHandler.cs
+++ AppleSceneEditor/ComponentPanelHandler.cs
@@ -165,7 +165,7 @@ namespace AppleSceneEditor
 
         private void AddINameStringEditor(IName name, int indentLevel)
         {
-            TextBox? strEditor = CreateStringEditor(name);
+            TextBox? strEditor = ValueEditorFactory.CreateStringEditor(name);
             if (strEditor is not null)
             {
                 strEditor.Margin = new Thickness(indentLevel + IndentationIncrement, 0, 0, 0);
@@ -301,7 +301,7 @@ namespace AppleSceneEditor
 
         private void AddProperty(int indentLevel, JsonProperty property)
         {
-            TextBox? nameBox = CreateStringEditor(property, true);
+            TextBox? nameBox = ValueEditorFactory.CreateStringEditor(property, true);
             if (nameBox is not null)
             {
                 nameBox.Margin = new Thickness(indentLevel, 0, 0, 0);
@@ -312,13 +312,13 @@ namespace AppleSceneEditor
             switch (property.ValueKind)
             {
                 case JsonValueKind.True or JsonValueKind.False:
-                    addWidget = CreateBooleanEditor(property);
+                    addWidget = ValueEditorFactory.CreateBooleanEditor(property);
                     break;
                 case JsonValueKind.String:
-                    addWidget = CreateStringEditor(property);
+                    addWidget = ValueEditorFactory.CreateStringEditor(property);
                     break;
                 case JsonValueKind.Number:
-                    addWidget = CreateNumericEditor(property);
+                    addWidget = ValueEditorFactory.CreateNumericEditor(property);
                     break;
             }
 
@@ -328,112 +328,6 @@ namespace AppleSceneEditor
             ValueStackPanel.AddChild(addWidget);
         }
 
-        //---------------------------
-        // Create editor methods
-        //---------------------------
-
-        private CheckBox? CreateBooleanEditor(JsonProperty property)
-        {
-            if (property.ValueKind is not JsonValueKind.False and not JsonValueKind.True)
-            {
-                Debug.WriteLine($"Value kind is not a boolean! Returning null. Value kind: {property.ValueKind}. " +
-                                $" Method: ({nameof(CreateBooleanEditor)})");
-                return null;
-            }
-
-            bool isChecked = (bool) (property.Value ?? false);
-            CheckBox checkBox = new() {IsPressed = isChecked};
-
-            checkBox.Click += (s, ea) => property.Value = checkBox.IsPressed;
-
-            return checkBox;
-        }
-
-        private TextBox? CreateStringEditor(JsonProperty property, bool changeName = false)
-        {
-            if (property.ValueKind != JsonValueKind.String && !changeName)
-            {
-                Debug.WriteLine($"{nameof(ComponentPanelHandler)}.{nameof(CreateStringEditor)} (JsonProperty): " +
-                                $"Value kind is not a string! Returning null. Value kind: {property.ValueKind}. ");
-                return null;
-            }
-
-            TextBox textBox = new()
-            {
-                Text = changeName ? property.Name : property.Value as string,
-                MaxWidth = 50,
-            };
-
-            textBox.TextChanged += (s, ea) =>
-            {
-                if (textBox.Text is null || textBox.Text.Length > MaxTextLength) return;
-
-                if (changeName) property.Name = textBox.Text;
-                else property.Value = textBox.Text;
-            };
-
-            return textBox;
-        }
-
-        private TextBox? CreateStringEditor(IName name)
-        {
-            if (name.Name is null)
-            {
-                Debug.WriteLine($"{nameof(ComponentPanelHandler)}.{nameof(CreateStringEditor)} (IName):" +
-                                $"parameter's name value is null! Cannot create string editor. Returning null.");
-                return null;
-            }
-
-            TextBox textBox = new()
-            {
-                Text = name.Name,
-                MaxWidth = 50,
-            };
-
-            textBox.TextChanged += (s, e) =>
-            {
-                if (textBox.Text is null || textBox.Text.Length > MaxTextLength) return;
-
-                name.Name = textBox.Text;
-            };
-
-            return textBox;
-        }
-
-        private SpinButton? CreateNumericEditor(JsonProperty property)
-        {
-            if (property.ValueKind != JsonValueKind.Number)
-            {
-                Debug.WriteLine($"Value kind is not a number! Returning null. Value kind: {property.ValueKind}. " +
-                                $" Method: ({nameof(CreateNumericEditor)})");
-                return null;
-            }
-
-            //property.Value should never be null under any circumstances but if it is set it to the default value of
-            //an int which in this case is zero.
-            property.Value ??= default(int);
-            Type propertyValueType = property.Value.GetType();
-
-            SpinButton spinButton = new()
-            {
-                Integer = propertyValueType.IsNumericInteger(),
-                Nullable = propertyValueType.IsNullablePrimitive(),
-                Value = property.Value != null
-                    ? (float) Convert.ChangeType(property.Value, typeof(float))
-                    : default(float?),
-            };
-
-            spinButton.ValueChanged += (s, a) =>
-            {
-                object? result = spinButton.Value != null
-                    ? Convert.ChangeType(spinButton.Value.Value, propertyValueType)
-                    : null;
-
-                property.Value = result;
-            };
-
-            return spinButton;
-        }
         //------------------
         // Enums and misc.
         //------------------
