5533799c815f2d76d2eb32875e0a93c67bb78075
qapples <793678@lcps.org>
2021-09-06 12:36:22 -0400

    Add documentation to ComponentWrapperExtensions.cs.

--- AppleSceneEditor/Extensions/ComponentWrapperExtensions.cs
+++ AppleSceneEditor/Extensions/ComponentWrapperExtensions.cs
@@ -9,6 +9,9 @@ using AppleSerialization.Json;
 
 namespace AppleSceneEditor.Extensions
 {
+    /// <summary>
+    /// Assists in working with instances that implement <see cref="IComponentWrapper"/>.
+    /// </summary>
     public static class ComponentWrapperExtensions
     {
         /// <summary>
@@ -18,9 +21,11 @@ namespace AppleSceneEditor.Extensions
         private static Dictionary<Type, Type> Implementers { get; }
 
         private const BindingFlags ActivatorFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+        
 
         private static Dictionary<Type, Type> LoadImplementers()
         {
+            string methodName = $"{nameof(ComponentWrapperExtensions)}.{nameof(LoadImplementers)}";
             Dictionary<Type, Type> outDictionary = new();
             IEnumerable<Type> assemblyTypes;
             Type wrapperInterface = typeof(IComponentWrapper);
@@ -49,12 +54,29 @@ namespace AppleSceneEditor.Extensions
                     continue;
                 }
 
+                if (outDictionary.ContainsKey(wrapper.AssociatedType))
+                {
+                    Debug.WriteLine($"{methodName}: cannot include {type} because another type has already " +
+                                    $"implemented {wrapper.AssociatedType}");
+                    continue;
+                }
+                
                 outDictionary.Add(wrapper.AssociatedType, type);
             }
 
             return outDictionary;
         }
 
+        /// <summary>
+        /// Creates a new object that implements <see cref="IComponentWrapper"/> with a specified type that it wraps
+        /// around.
+        /// </summary>
+        /// <param name="jsonObject">The <see cref="JsonObject"/> instance that contains the data the wrapper will work
+        /// with.</param>
+        /// <param name="type">The <see cref="Type"/> that <see cref="jsonObject"/> has.</param>
+        /// <returns>If there is a wrapper class associated with the specified type (<see cref="type"/>), then a new
+        /// <see cref="IComponentWrapper"/> instance referencing that wrapper class is returned. Otherwise, null is
+        /// returned.</returns>
         public static IComponentWrapper? CreateFromType(JsonObject jsonObject, Type? type)
         {
             if (type is null) return null;
@@ -80,6 +102,16 @@ namespace AppleSceneEditor.Extensions
                 args: new[] {jsonObject}, null) as IComponentWrapper;
         }
 
+        /// <summary>
+        /// Creates a new object that implements <see cref="IComponentWrapper"/> with a specified type (in string form)
+        /// that it wraps around.
+        /// </summary>
+        /// <param name="jsonObject">The <see cref="JsonObject"/> instance that contains the data the wrapper will work
+        /// with.</param>
+        /// <param name="typeName">The name of the type that <see cref="jsonObject"/> has.</param>
+        /// <returns>If <see cref="typeName"/> is valid and if there is a wrapper class associated with that type, then
+        /// a new <see cref="IComponentWrapper"/> instance referencing that wrapper class is returned. Otherwise, null
+        /// is returned.</returns>
         public static IComponentWrapper? CreateFromType(JsonObject jsonObject, string typeName) =>
             CreateFromType(jsonObject, ConverterHelper.GetTypeFromString(typeName));
 
