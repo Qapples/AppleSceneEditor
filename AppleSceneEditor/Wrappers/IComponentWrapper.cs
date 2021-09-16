45588e7b04c0c13e17c0d32ac99fae37b69969a1
qapples <793678@lcps.org>
2021-09-16 15:00:25 -0400

    Update docs in IComponentWrapper.cs.

--- AppleSceneEditor/Wrappers/IComponentWrapper.cs
+++ AppleSceneEditor/Wrappers/IComponentWrapper.cs
@@ -6,11 +6,13 @@ namespace AppleSceneEditor.Wrappers
 {
     /// <summary>
     /// Represents an object that wraps around a <see cref="JsonObject"/> of a specified type and exposes data that
-    /// the user can manipulate through the property editor. In order to create instances, use
+    /// the user can manipulate through the property editor.
+    /// <remarks>In order to create instances, use
     /// <see cref="AppleSceneEditor.Extensions.ComponentWrapperExtensions.CreateFromType(AppleSerialization.Json.JsonObject, string)"/>
     /// or <br/>
     /// <see cref="AppleSceneEditor.Extensions.ComponentWrapperExtensions.CreateFromType(AppleSerialization.Json.JsonObject, Type)"/>.
     /// <br/> Do not attempt to use constructors.
+    /// </remarks>
     /// </summary>
     public interface IComponentWrapper
     {
@@ -21,13 +23,13 @@ namespace AppleSceneEditor.Wrappers
 
         /// <summary>
         /// <see cref="Panel"/> containing <see cref="Widget"/> instances that is displayed to the user in the property
-        /// editor.
+        /// editor and additional occasions.
         /// </summary>
         Panel UIPanel { get; set; }
         
         /// <summary>
-        /// If true, then there is no data associated with this wrapper or creation of the the wrapper failed. Otherwise
-        /// ,false. <br/>
+        /// If true, then there is no data associated with this wrapper or creation of the the wrapper failed. Otherwise,
+        /// false.
         /// </summary>
         bool IsEmpty { get; }
         
