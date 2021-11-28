ade5045a93a857f57dd30a5e4c491b4fbe7312e9
qapples <793678@lcps.org>
2021-11-28 13:48:46 -0500

    Update the position of the complexbox (if it exists) along with the transform in ChangeTransformCommand

--- AppleSceneEditor/Commands/ChangeTransformCommand.cs
+++ AppleSceneEditor/Commands/ChangeTransformCommand.cs
@@ -1,4 +1,5 @@
 using DefaultEcs;
+using GrappleFightNET5.Components.Collision;
 using GrappleFightNET5.Components.Transform;
 
 namespace AppleSceneEditor.Commands
@@ -8,8 +9,8 @@ namespace AppleSceneEditor.Commands
         public bool Disposed { get; private set; }
 
         private readonly Entity _entity;
-        private readonly Transform _oldTransform;
-        private readonly Transform _newTransform;
+        private Transform _oldTransform;
+        private Transform _newTransform;
 
         public ChangeTransformCommand(Entity entity, Transform oldTransform, Transform newTransform)
         {
@@ -18,15 +19,25 @@ namespace AppleSceneEditor.Commands
 
         public void Execute()
         {
-            _entity.Set(_newTransform);
+            MoveEntity(ref _newTransform);
         }
 
         public void Undo()
         {
-            _entity.Set(_oldTransform);
+            MoveEntity(ref _oldTransform);
         }
 
         public void Redo() => Execute();
+
+        private void MoveEntity(ref Transform transform)
+        {
+            _entity.Set(transform);
+
+            if (_entity.Has<ComplexBox>())
+            {
+                _entity.Get<ComplexBox>().Center = transform.Matrix.Translation;
+            }
+        }
 
         public void Dispose()
         {
